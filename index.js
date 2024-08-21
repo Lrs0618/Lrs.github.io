626b68bd47f6abe919d8794fab59ae06b1626bdd
Lrs <qkliangfeng@qq.com>
2024-08-21 12:43:47 +0800

    试试网络请求

---
+++ index.js
@@ -0,0 +1,15 @@
+function fetchData() {
+    fetch('https://tea.qingnian8.com/api/bizhi/homeBanner', {
+        headers: {
+            'access-key': 'Lrs_key@xxm2024'
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const result = document.getElementById('result');
+            result.innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
